f4c6d5a18a2bd7db4219d0fd19cd112de1906a86
kiedveian <kiedveian@yahoo.com.tw>
2020-09-12 11:02:12 +0800

    Parse some command

--- fops/fops.go
+++ fops/fops.go
@@ -4,10 +4,29 @@ import(
     "fmt"
 )
 
-func Test(){
-    fmt.Println("hello world")
+func fops(args []string){
+//    fmt.Printf("len=%d cap=%d %v\n", len(args), cap(args), args)
+    if len(args)>=1{
+        switch cmd := args[0]; cmd{
+        case "help":
+            help(args)
+        case "linecount":
+            linecount(args)
+        default:
+            fmt.Println("undefined command ", cmd)
+        }
+    }
+}
+
+func help(args []string){
+    fmt.Println("test help command")
+}
+
+func linecount(args []string){
+    fmt.Println("test linecount command")
 }
 
 func main(){
-    Test()
+    fops([]string{"help"})
+    fops([]string{"linecount"})
 }
